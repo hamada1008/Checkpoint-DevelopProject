d1338c5c276fcf0cfb64697b36cb1b6c0708a869
Houssemeddine SALEM <salemhoussemeddine@gmail.com>
2021-09-10 17:53:33 +0100

    adding orderRouter

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 // import nanny from "./models/nanny.js";
 // import parent from "./models/parent.js";
-import {} from "dotenv/config";
+import { } from "dotenv/config";
 import authRouter from "./router/authRouter.js";
 import settingRouter from "./router/settingRouter.js";
 import searchRouter from "./router/searchRouter.js";
+import orderRouter from "./router/orderRouter.js"
 
 const app = Express();
 app.use(Express.json());
@@ -14,6 +15,7 @@ app.use(Express.urlencoded({ extended: true }));
 app.use("/api", authRouter);
 app.use("/api", settingRouter);
 app.use("/api", searchRouter);
+app.use("/api", orderRouter);
 const port = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URL)
