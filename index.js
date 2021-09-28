ad1b1688824fb9bb30f74fe86d483a47a1419eeb
Oussama <oussamahamada@yahoo.com>
2021-09-28 23:37:05 +0100

    my best nanny final

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use("/api", searchRouter);
 app.use("/api", orderRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(Express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
